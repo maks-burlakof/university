da50fd7aa82a93cc8df0e21b09a26871cff29c36
Maksim Burlakov <maks.burlakof@gmail.com>
2022-03-03 10:34:58 +0300

    modified lab2.2

--- sem2/lab2.2.cpp
+++ sem2/lab2.2.cpp
@@ -1,21 +1,67 @@
-﻿#include <iostream> // Индив на листе
+﻿#include <iostream>
 #include <conio.h>
 using namespace std;
 
-int* recurs(int *arr, int &k, int &kol, int &poradok) {
+int* recurs(int*, int&, int&, int&);
+int* non_recurs(int&, int&, int&);
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    srand(time(0));
+    int n, k, kol;
+    while (1) {
+        cout << "\nКоличество камней:\nБуква для случайного выбора\n0 - Выход\n>>> ";
+        cin >> n;
+        if (n == 0) break;
+        if (!cin) {
+            cin.clear();
+            while (cin.get() != '\n');
+            n = rand() % 30 + 10;    // 10..40
+            cout << n << "\n";
+        }
+        cout << "Введите k: \n>>> ";
+        while (!(cin >> k)) {
+            cout << "Введите целое число: \n";
+            cin.clear();
+            while (cin.get() != '\n');
+        }
+
+        cout << "\n1 - Использовать рекурсивную функцию\n2 - Использовать обычную функцию\n\n";
+        switch (_getch()) {
+        case '1': {
+            cout << "Использована рекурсивная функция.\nОтвет: ";
+            int* first_arr = new int[n];
+            for (int i = 1; i <= n; i++) first_arr[i - 1] = i; //формируем массив
+            kol = n;
+            int poradok = 0;
+            int* arr = recurs(first_arr, k, kol, poradok);
+            for (int i = 0; i < kol; i++) cout << arr[i] << " ";
+            cout << "\nОсталось камней: " << kol << endl;
+            delete[] arr;
+            break;
+        }
+        default: {
+            cout << "Использована обычная функция.\nОтвет: ";
+            int* arr = non_recurs(n, k, kol);
+            for (int i = 0; i < kol; i++) cout << arr[i] << " ";
+            cout << "\nОсталось камней: " << kol << endl;
+            delete[] arr;
+            break;
+        }
+        }
+    }
+    return 0;
+}
+
+int* recurs(int* arr, int& k, int& kol, int& poradok) {
     if (kol < k) return arr;
     for (int i = 0; i < kol; i++) {
         poradok++;
-        //cout << "poradok = " << poradok << "\n";//
-        if (poradok == k) { //удалить элемент, сдвинуть массив, kol--, poradok = 0.
-            //cout << "Удален элемент " << arr[i] << "\n";//
+        if (poradok == k) {
             for (int j = i; j < kol - 1; j++) arr[j] = arr[j + 1];
             kol--;
             i--;
             poradok = 0;
-            //cout << "Остались элементы:\n";//
-            //for (int i = 0; i < kol; i++) cout << arr[i] << " ";//
-            //cout << "\n";//
         }
     }
     return recurs(arr, k, kol, poradok);
@@ -24,75 +70,18 @@ int* recurs(int *arr, int &k, int &kol, int &poradok) {
 int* non_recurs(int& n, int& k, int& kol) {
     kol = n;
     int poradok = 0;
-    int *arr = new int[n];
-    for (int i = 1; i <= n; i++) arr[i-1] = i;
+    int* arr = new int[n];
+    for (int i = 1; i <= n; i++) arr[i - 1] = i;
     while (kol >= k) {
-        //cout << "Новый проход цикла\nОстались элементы:\n";//
-        //for (int i = 0; i < kol; i++) cout << arr[i] << " ";//
-        //cout << "\n";//
         for (int i = 0; i < kol; i++) {
             poradok++;
-            //cout << "poradok = " << poradok << "\n";//
             if (poradok == k) {
-                //cout << "Удален элемент " << arr[i] << "\n";//
-                for (int j = i; j < kol-1; j++) arr[j] = arr[j+1];
+                for (int j = i; j < kol - 1; j++) arr[j] = arr[j + 1];
                 kol--;
                 i--;
                 poradok = 0;
-                //cout << "Остались элементы:\n";//
-                //for (int i = 0; i < kol; i++) cout << arr[i] << " ";//
-                //cout << "\n";//
             }
         }
     }
-    //cout << "Выход из while";//
-    //cout << "Ответ: kol = " << kol << ", остались элементы:\n";//
-    //for (int i = 0; i < kol; i++) cout << arr[i] << " ";//
-    //cout << "\n";//
     return arr;
-}
-
-int main() {
-    setlocale(LC_ALL, "Russian");
-    srand(time(0));
-    int n, k, kol;
-    cout << "Количество камней:\nИли буква для случайного выбора\n";
-    cin >> n;
-    if (!cin) {
-        cin.clear();
-        while (cin.get() != '\n');
-        n = rand() % 30 + 10;    // 10..40
-        cout << n << "\n";
-    }
-    cout << "Введите k: ";
-    while (!(cin >> k)) {
-        cout << "Введите целое число: \n";
-        cin.clear();
-        while (cin.get() != '\n');
-    }
-    
-    cout << "\n1 - Использовать рекурсивную функцию\n2 - Использовать обычную функцию\n\n";
-    switch (_getch()) {
-    case '1': {
-        cout << "Использована рекурсивная функция.\nОтвет: ";
-        int* first_arr = new int[n];
-        for (int i = 1; i <= n; i++) first_arr[i-1] = i; //формируем массив
-        kol = n;
-        int poradok = 0;
-        int* arr = recurs(first_arr, k, kol, poradok);
-        for (int i = 0; i < kol; i++) cout << arr[i] << " ";
-        cout << "\nОсталось камней: " << kol << endl;
-        delete[] arr;
-        break;
-    }
-    default: {
-        cout << "Использована обычная функция.\nОтвет: ";
-        int* arr = non_recurs(n, k, kol);
-        for (int i = 0; i < kol; i++) cout << arr[i] << " ";
-        cout << "\nОсталось камней: " << kol << endl;
-        delete[] arr;
-        break;
-    }
-    }
-    return 0;
 }
