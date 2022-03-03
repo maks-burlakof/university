2b6ce4e1ee0e60f54266afaefbad39de99aa588d
Maksim Burlakov <maks.burlakof@gmail.com>
2022-03-03 10:08:22 +0300

    modified lab1.1

--- sem2/lab1.1.cpp
+++ sem2/lab1.1.cpp
@@ -2,8 +2,9 @@
 #include <conio.h>
 using namespace std;
 
-void summa(int*, int, int*);
-void proizv(int*, int, int&);
+//void summa(int*, int, int*);
+//void proizv(int*, int, int&);
+void poisk(int*, int, int*, int&);
 
 int main()
 {
@@ -21,7 +22,7 @@ int main()
     switch (_getch()) {
     case '1':
         cout << "Заполните массив из " << n << " элементов:\n\t";
-        for (int i = 0; i < n; i++) cin >> arr[i];
+        for (int i = 0; i < n; i++) cin >> *(arr + i);
         break;
     default:
         cout << "Введите диапазон значений случайных чисел [a, b]: ";
@@ -29,24 +30,34 @@ int main()
         cin >> a >> b;
         srand(time(0));
         for (int i = 0; i < n; i++) {
-            arr[i] = rand() % (b + 1 - a) + a;
-            cout << arr[i] << "  ";
+            *(arr + i) = rand() % (b + 1 - a) + a;
+            cout << *(arr + i) << "  ";
         }
         break;
     }
-    int sum = 0, proiz = 1;
-    summa(arr, n, &sum);
-    cout << "\nСумма положительных элементов: " << sum;
-    proizv(arr, n, proiz);
-    cout << "\nПроизведение отрицательных элементов: " << proiz << endl;
+    int sum = -1, proiz = 0;
+    poisk(arr, n, &sum, proiz);
+    if (sum < 0) cout << "\nПоложительных элементов нет"; else cout << "\nСумма положительных элементов: " << sum;
+    if (proiz == 0) cout << "\nОтрицательных элементов нет"; else cout << "\nПроизведение отрицательных элементов: " << proiz << endl;
     delete[] arr;
     return 0;
 }
 
-void summa(int *arr, int n, int *sum) {
-    for (int i = 0; i < n; i++) if (arr[i] > 0) *sum += arr[i];
-}
+//void summa(int *arr, int n, int *sum) {
+//    for (int i = 0; i < n; i++) if (*(arr + i) > 0) *sum += *(arr + i);
+//}
+//
+//void proizv(int *arr, int n, int &proiz) {
+//    for (int i = 0; i < n; i++) if (*(arr + i) < 0) proiz *= *(arr + i);
+//}
 
-void proizv(int *arr, int n, int &proiz) {
-    for (int i = 0; i < n; i++) if (arr[i] < 0) proiz *= arr[i];
+void poisk(int* arr, int n, int* sum, int& proiz) {
+    for (int i = 0; i < n; i++) {
+        if (*(arr + i) > 0) if (*sum < 0) *sum += *(arr + i) + 1; else *sum += *(arr + i);
+        if (*(arr + i) < 0) if (proiz == 0) {
+            proiz = 1;
+            proiz *= *(arr + i);
+        }
+        else proiz *= *(arr + i);
+    }
 }
