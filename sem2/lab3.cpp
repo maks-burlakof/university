10c383b3555b0945a5f3243e13f0da75cc442ca5
Maksim Burlakov <maks.burlakof@gmail.com>
2022-04-11 20:11:04 +0300

    add lab3.cpp
    Все работает, частично кроме индивидуалки. Если в вершине стека нечетное число - все окей. Если в вершине четное, которое нужно удалить, прога крашится

---
+++ sem2/lab3.cpp
@@ -0,0 +1,146 @@
+// Обязательно добавить проверки
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+struct Stack {
+	int info;
+	Stack *next, *begin;
+};
+
+Stack *add (Stack*, int);
+void view (Stack*);
+void del(Stack**);
+void task(Stack*);
+
+int main() {
+	Stack *begin = NULL;
+	int choice, n, a, b, in;
+	while (true) {
+		cout << "--------- MENU ---------\n";
+		cout << "1 - Create\n2 - Add\n3 - View\n"
+			"4 - Individual task\n5 - Delete\n0 - EXIT\n";
+		cout << "-------------------------\n >>> ";
+		cin >> choice;
+		system("cls");
+		switch (choice) {
+		case 1:
+		case 2:
+			if (begin != NULL && choice == 1) {
+				cout << "The stack is not empty!\nClear memory!\n";
+				break;
+			}
+			cout << " >>> Amount of elements = ";
+			cin >> n;
+			cout << " >>> [a, b] = ";
+			cin >> a >> b;
+			srand(time(0));
+			for (int i = 1; i <= n; i++) {
+				in = rand() % (b + 1 - a) + a;
+				begin = add(begin, in); // вершину стека ставим на новый элемент
+			}
+			cout << "Success!\n";
+			break;
+		case 3:
+			if (!begin) {
+				cout << "Stack is empty!\n";
+				break;
+			}
+			cout << "--- STACK ---\n";
+			view(begin);
+			break;
+		case 4:
+			if (!begin) {
+				cout << "Stack is empty!\n";
+				break;
+			}
+			task(begin);
+			break;
+		case 5:
+			del(&begin);
+			cout << "Stack deleted!\n";
+			break;
+		case 0:
+			if (begin != NULL)
+				del(&begin);
+			return 0;
+		}
+	}
+}
+
+Stack *add(Stack *p, int element) {
+	Stack *t = new Stack;
+	t->info = element;
+	t->next = p; // записываем адрес предыдущего элемента
+	return t;
+}
+
+void view(Stack *p) {
+	Stack *t = p;
+	while (t) {
+		cout << t->info << endl;
+		t = t->next; // Переставляем текущий указатель t на следующий за ним элемент
+	}
+}
+
+void del(Stack **p) {
+	Stack *t;
+	while (*p) {
+		t = *p; // Устанавливаем текущий указатель на вершину
+		*p = (*p)->next; // Вершину переставляем на следующий элемент
+		delete t; // Освобождаем память бывшей вершины
+	}
+}
+
+//void task(Stack *p) {
+//	Stack *t = p;
+//	Stack *t2;
+//	while (t) {
+//		if (t->info % 2 == 0) {
+//			t2 = t;
+//			delete t;
+//			t = t2;
+//		}
+//		t = t->next;
+//	}
+//}
+
+void task(Stack *p) {
+	Stack* t = p;
+	Stack* t1 = p;
+	while (t) {
+		if (t->info % 2 != 0) {
+			t1 = t;
+			t = t->next;
+		}
+		else {
+			t1->next = t->next;
+			delete t;
+			t = t1->next;
+		}
+	}
+}
+
+//Stack* task(Stack* p) {
+//	Stack* t = p;
+//	Stack* t1 = p;
+//	while (t) {
+//		if (t->info % 2 != 0) {
+//			t1 = t;
+//			t = t->next;
+//		}
+//		else {
+//			if (t == p) {
+//				p = p->next;
+//				delete t;
+//				return p;
+//			}
+//			else {
+//				t1->next = t->next;
+//				delete t;
+//				t = t1->next;
+//			}
+//		}
+//	}
+//}
